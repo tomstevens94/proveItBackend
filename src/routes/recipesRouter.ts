0fd28616aee690ca94a7ec11a4ee9832434bc824
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2023-12-08 09:30:27 +0000

    🔧 TS config setup

--- src/routes/recipesRouter.ts
+++ src/routes/recipesRouter.ts
@@ -1,26 +1,26 @@
-import express from 'express';
+import express from "express";
 import {
   getAllRecipes,
   getRecipeById,
   postRecipeIsComplete,
-  postNewRecipe,
-} from '../controllers/recipesController';
-import multer from 'multer';
+  // postNewRecipe,
+} from "../controllers/recipesController";
+// import multer from 'multer';
 
-let storage = multer.diskStorage({
-  destination: 'uploads/',
-  filename: function (req, file, cb) {
-    cb(null, `${file.originalname}.${file.mimetype.split('/')[1]}`);
-  },
-});
+// let storage = multer.diskStorage({
+//   destination: 'uploads/',
+//   filename: function (req, file, cb) {
+//     cb(null, `${file.originalname}.${file.mimetype.split('/')[1]}`);
+//   },
+// });
 
-const upload = multer({ storage }).single('imageData');
+// const upload = multer({ storage }).single('imageData');
 
 const router = express.Router();
 
-router.get('/', getAllRecipes);
-router.get('/:recipeId', getRecipeById);
-router.post('/complete', postRecipeIsComplete);
-router.post('/new', upload, postNewRecipe);
+router.get("/", getAllRecipes);
+router.get("/:recipeId", getRecipeById);
+router.post("/complete", postRecipeIsComplete);
+// router.post("/new", upload, postNewRecipe);
 
 export default router;
