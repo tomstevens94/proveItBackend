061d67a00a8ff7473e18aea94876a34d3b6047c7
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2023-12-18 14:16:26 +0000

    🔧 firebase key JSON.parse

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,12 +1,14 @@
-import 'dotenv/config';
-import { credential, ServiceAccount } from 'firebase-admin';
+import "dotenv/config";
+import { credential, ServiceAccount } from "firebase-admin";
 
 const { FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL } =
   process.env;
 
 const firebaseServiceAccount: ServiceAccount = {
   projectId: FIREBASE_PROJECT_ID,
-  privateKey: FIREBASE_PRIVATE_KEY,
+  privateKey: FIREBASE_PRIVATE_KEY
+    ? JSON.parse(FIREBASE_PRIVATE_KEY)
+    : undefined,
   clientEmail: FIREBASE_CLIENT_EMAIL,
 };
 
