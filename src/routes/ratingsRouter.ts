2988fdecb4b15f81c4345572185b8fc06060f696
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2024-01-19 11:52:04 +0000

    ⚰️ Remove ratings routes/controllers

--- src/routes/ratingsRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { postRecipeRating } from '../controllers/ratingsController';
-
-const router = express.Router();
-
-router.post('/rateRecipe', postRecipeRating);
-
-export default router;
