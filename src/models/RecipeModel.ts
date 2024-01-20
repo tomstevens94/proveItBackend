6b2de984036f89cad3f9620744e95bef3d4e0d6e
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2024-01-20 11:13:02 +0000

    🏷️ Update recipeSchema to contain array of images

--- src/models/RecipeModel.ts
+++ src/models/RecipeModel.ts
@@ -1,10 +1,15 @@
 import mongoose, { Schema } from "mongoose";
 
+const recipeImageSchema = new Schema({ url: { type: String, required: true } });
+
 const recipeSchema = new Schema({
   title: String,
   createdByUserId: String,
   difficulty: String,
-  imageUrl: String,
+  images: {
+    type: [recipeImageSchema],
+    required: true,
+  },
   categories: [String],
   duration: {
     unit: String,
