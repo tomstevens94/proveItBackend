c0d285dee0707cb012bd52384609ec12639d8ece
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2024-05-28 11:25:09 +0100

    ✨ User/avatar schema

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -5,13 +5,30 @@ const ratedRecipeSchema = new Schema({
   rating: { type: Number, required: true },
 });
 
+const avatarSchema = new Schema({
+  type: { type: String, required: true },
+  options: {
+    hair: { type: [String], required: true },
+    face: { type: [String], required: true },
+    mouth: { type: [String], required: true },
+    ear: { type: [String], required: true },
+    eyes: { type: [String], required: true },
+    cheek: { type: [String], required: true },
+    cheekProbability: { type: Number, required: true },
+    nose: { type: [String], required: true },
+    sideburn: { type: [String], required: true },
+    frontHair: { type: [String], required: true },
+    hairColor: { type: [String], required: true },
+    skinColor: { type: [String], required: true },
+  },
+});
+
 const userSchema = new Schema({
   userId: { type: String, required: true },
-  completedRecipes: { type: [Schema.Types.ObjectId], required: true },
-  ratedRecipes: { type: [ratedRecipeSchema], required: true },
   photoUrl: { type: String },
   firstName: { type: String },
   lastName: { type: String },
+  avatar: { type: avatarSchema, required: false },
 });
 
 export default mongoose.model("User", userSchema);
