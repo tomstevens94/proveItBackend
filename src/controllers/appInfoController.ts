d5742f4e82b8ac1cab23eaca1aa4270a491147eb
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2024-07-10 12:51:04 +0100

    🔖 Increment min version

--- src/controllers/appInfoController.ts
+++ src/controllers/appInfoController.ts
@@ -4,7 +4,7 @@ import { HTTPStatusCodes } from "../configs/HTTPStatusCodes";
 export const getAppInfo: RequestHandler = async (req, res) => {
   try {
     return res.status(HTTPStatusCodes.OK).json({
-      minSupportedVersion: "1.0.14",
+      minSupportedVersion: "1.0.21",
     });
   } catch (err) {
     console.log("Error getting app info", err);
