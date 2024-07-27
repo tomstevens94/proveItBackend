6584ad1e5969ad5cfe417c900cf937e5c9d01e1e
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2024-07-27 14:28:50 +0100

    👔 Increment version/app URL response

--- src/controllers/appInfoController.ts
+++ src/controllers/appInfoController.ts
@@ -4,7 +4,11 @@ import { HTTPStatusCodes } from "../configs/HTTPStatusCodes";
 export const getAppInfo: RequestHandler = async (req, res) => {
   try {
     return res.status(HTTPStatusCodes.OK).json({
-      minSupportedVersion: "1.0.23",
+      minSupportedVersion: "1.1.0",
+      appUrl: {
+        ios: "https://www.apple.com/app-store/",
+        android: "https://play.google.com/store/",
+      },
     });
   } catch (err) {
     console.log("Error getting app info", err);
