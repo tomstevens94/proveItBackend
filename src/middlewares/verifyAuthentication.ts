581caa5f0e3e62485b6cc883e033a1fcdf3e0f2e
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2024-07-30 15:03:22 +0100

    💩 Fix apple guideline issues

--- src/middlewares/verifyAuthentication.ts
+++ src/middlewares/verifyAuthentication.ts
@@ -8,6 +8,15 @@ export const verifyAuthentication: RequestHandler = async (req, res, next) => {
 
   const idToken = req.headers.authorization;
 
+  // Apple required users to not have to reenter name after sign in
+  // TODO: Replace this middleware with an actual 'createAccountController'
+  // This works but is not good
+  const appleFullNameHeader = req.headers.applefullname;
+  const appleFullName =
+    appleFullNameHeader && typeof appleFullNameHeader === "string"
+      ? JSON.parse(appleFullNameHeader)
+      : undefined;
+
   if (!idToken) {
     console.log("Unable to verify - No ID token supplied in request");
     return res.sendStatus(HTTPStatusCodes.Unauthorized);
@@ -23,13 +32,19 @@ export const verifyAuthentication: RequestHandler = async (req, res, next) => {
       console.log("Verified existing user");
     } else {
       console.log("New user, attempting to create in db");
-      await UserModel.create({
+
+      const userPayload = {
         userId,
         completedRecipes: [],
         ratedRecipes: [],
         photoUrl: userRecord.photoURL,
         displayName: userRecord.displayName,
-      });
+        firstName: appleFullName?.firstName,
+        lastName: appleFullName?.lastName,
+      };
+
+      await UserModel.create(userPayload);
+
       console.log("New user created with ID:", userId);
     }
 
