9a88515153efbe539532dd5759e2929528936366
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2025-01-17 09:13:05 +0000

    🛂 Socket auth verification

---
+++ src/middlewares/socket.ts
@@ -0,0 +1,29 @@
+import { ExtendedError, Socket } from "socket.io";
+import { verifyAuthenticatedIdToken } from "../utils/verifyAuthenticatedIdToken";
+import UserModel from "../models/UserModel";
+
+export const verifySocketAuthentication = async (
+  socket: Socket,
+  next: (err?: ExtendedError) => void
+) => {
+  try {
+    const idToken = socket.handshake.auth.idToken;
+    if (!idToken) {
+      throw new Error("No ID token within socket handshake");
+    }
+
+    const userRecord = await verifyAuthenticatedIdToken(idToken);
+    const { uid: userId } = userRecord;
+
+    const existingUser = await UserModel.exists({ userId });
+
+    if (existingUser) {
+      socket.handshake.auth.userId = userId;
+      return next();
+    } else {
+      throw new Error("User not found");
+    }
+  } catch (err: any) {
+    return next(new Error(`Unable to verify IDToken within socket - ${err}`));
+  }
+};
