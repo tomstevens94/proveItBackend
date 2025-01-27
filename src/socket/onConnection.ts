c0795b07cc6a4f3fb9ff5d56cbea55f2a00bb283
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2025-01-27 22:22:00 +0000

    👔 handle rejoin

--- src/socket/onConnection.ts
+++ src/socket/onConnection.ts
@@ -4,6 +4,7 @@ import { Socket } from "socket.io";
 import { onMessage } from "./onMessage";
 import { onDisconnect } from "./onDisconnect";
 import { createAssistant } from "../ai/assistantInstance";
+import { onRejoinConversation } from "./onRejoinConversation";
 
 export const onConnection = async (socket: Socket) => {
   const userId = socket.handshake.auth.userId;
@@ -19,6 +20,9 @@ export const onConnection = async (socket: Socket) => {
   console.log("UserID: ", userId, " connected");
 
   socket.on("message", (message) => onMessage(socket, message));
+  socket.on("rejoin_conversation", (threadId) =>
+    onRejoinConversation(socket, threadId)
+  );
   socket.on("disconnect", (disconnectReason) =>
     onDisconnect(socket, disconnectReason)
   );
