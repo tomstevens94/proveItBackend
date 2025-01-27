00fc19ec6da175871dbd74bda444e095c19b737e
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2025-01-27 22:21:19 +0000

    ✨ Generate conversation name/store conversation

--- src/socket/onMessage.ts
+++ src/socket/onMessage.ts
@@ -2,10 +2,25 @@ import { Socket } from "socket.io";
 import { assistantInstance } from "../ai/assistantInstance";
 import { openAiInstance } from "../ai/openAiInstance";
 import { getThreadIdFromSocket } from "../utils/socket/getThreadIdFromSocket";
+import ChatConversationModel from "../models/ChatConversationModel";
+import { v4 as uuidv4 } from "uuid";
+import { getNowISO } from "../controllers/chat/getNewConversation";
 
 export const onMessage = async (socket: Socket, payload: any) => {
   try {
+    const userId = socket.handshake.auth.userId;
+
+    if (!userId || typeof userId !== "string") {
+      console.log("Invalid userId");
+      socket.disconnect();
+      return;
+    }
+
     const threadId = await getThreadIdFromSocket(socket);
+    const existingMessagesWithinThread =
+      await openAiInstance.beta.threads.messages.list(threadId);
+
+    const isNewConversation = existingMessagesWithinThread.data.length === 0;
 
     console.log("Message received: ", payload);
 
@@ -16,22 +31,33 @@ export const onMessage = async (socket: Socket, payload: any) => {
       content: payload.content.text,
     });
 
+    console.log("THREAD Created");
+
     const run = await openAiInstance.beta.threads.runs.createAndPoll(threadId, {
       assistant_id: assistantInstance.id,
     });
 
     if (run.status === "completed") {
+      console.log("Run complete");
+
       const messagesResponse = await openAiInstance.beta.threads.messages.list(
         run.thread_id
+      );
+
+      console.log(
+        "Response recieved: ",
+        messagesResponse.data.map((e) =>
+          e.content[0].type === "text" ? e.content[0].text : ""
+        )
       );
 
       const messages = messagesResponse.data
         .reverse()
         .map((messageResponse) => ({
-          userId:
+          user:
             messageResponse.role === "assistant"
-              ? "AI"
-              : socket.handshake.auth.userId,
+              ? { role: "assistant" }
+              : { role: "user", userId: socket.handshake.auth.userId },
           content: {
             type: "text",
             text: messageResponse.content
@@ -42,6 +68,42 @@ export const onMessage = async (socket: Socket, payload: any) => {
           },
         }));
       socket.emit("update_messages", messages);
+
+      if (isNewConversation) {
+        // Generate conversation name
+        // // Create and store new conversation
+        const conversationNameCompletion =
+          await openAiInstance.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "system",
+                content: `Generate a user-facing name to summarise this conversation between the user and an assistant, in 5 words or less, without any wrapping punctuation or: \n\n${JSON.stringify(
+                  messages
+                )}`,
+              },
+            ],
+          });
+
+        const conversationName =
+          conversationNameCompletion.choices[0].message.content;
+        console.log("Conversation name", conversationName);
+
+        console.log("Storing new conversation");
+
+        const newConversation = {
+          id: uuidv4(),
+          threadId,
+          name: conversationName,
+          createdOn: getNowISO(),
+          lastUpdatedOn: getNowISO(),
+          usersData: [{ userId }, { userId: "assistant" }],
+        };
+
+        await ChatConversationModel.create(newConversation);
+
+        socket.emit("invalidate_conversations");
+      }
     } else {
       console.log(run.status);
     }
