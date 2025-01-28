830a819d5a6430bd3b8765677499035c2dc2d9db
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2025-01-28 09:00:22 +0000

    🐛 Remove controller

--- src/routes/aiRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { aiController } from "../controllers/aiController";
-
-const router = express.Router();
-
-router.post("/", aiController);
-
-export default router;
